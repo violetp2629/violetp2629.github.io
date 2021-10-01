ce227534e89c5d8505c98d51d714e7027d08db46
violetp2629 <91745204+violetp2629@users.noreply.github.com>
2021-10-01 11:19:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Vio Patt"
+let userName = "violetp2629";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
